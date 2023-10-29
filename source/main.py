b14a0c564e43f1a91d2043d891561d70f12d64ec
Tucker Traywick <tucker.traywick@gmail.com>
2023-10-29 04:24:18 +0000

    Sync.

--- source/main.py
+++ source/main.py
@@ -1,35 +1,69 @@
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Optional
 
 
-class NodeType(Enum):
+# Represents the type of a token.
+class TokenType(Enum):
     INVALID = auto()
     NUMBER = auto()
     CHARACTER = auto()
     STRING = auto()
     IDENTIFIER = auto()
+    PLUS = auto()
+    MINUS = auto()
 
 
+# Represents a token. Maps to a segment of the input text.
 @dataclass
-class Node:
-    type: NodeType
-    textStart: int
-    texdEnd: int
-    parent: int
-    firstChild: int
-    lastChild: int
+class Token:
+    type: TokenType # The type of the token.
+    start: int # The index of the start of the token in the text.
+    end: int # The index of the end of the token in the text.
 
 
-@dataclass
-class ParsingResult:
-    errorMessage: Optional[str]
-    nodes: Optional[list[Node]]
-    topNode: Optional[Node]
+# Splits a string into tokens.
+def lex(text: str) -> list[Token]:
+    # Every non-alphanumeric character.
+    symbols = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+    # A map of token strigs to their operator types.
+    operators = {
+        "+": TokenType.PLUS,
+        "-": TokenType.MINUS,
+    }
+
+    tokens: list[Token] = []
+    currentToken: Token = None
+    for i, char in enumerate(text):
+        # If not currently lexing a token, figure out the type of the current token.
+        if currentToken is None:
+            if char.isspace():
+                continue
+            elif char.isdigit():
+                currentToken = Token(TokenType.NUMBER, i, i)
+            elif char.isalpha() or char == "_":
+                currentToken = Token(TokenType.IDENTIFIER, i, i)
+            elif char in symbols:
+                currentToken = Token(None, i, i)
+            else:
+                currentToken = Token(TokenType.INVALID, i, i)
+        # If in the middle of lexing a token, try to keep lexing it.
+        elif currentToken.type == TokenType.NUMBER and char.isdigit() \
+             or currentToken.type == TokenType.IDENTIFIER and (char.isalnum() or char == '_') \
+             or currentToken.type == None and (char in symbols):
+                currentToken.end = i
+            
+
+# Splits the file at `path` into tokens.
+def lexFile(path: str) -> list[Token]:
+    file = open(path, "r")
+    text = file.read()
+    file.close()
+    return lex(text)
 
 
 def main():
-    pass
+    path = "source/example.txt"
+    print(lexFile(path))
 
 
 if __name__ == "__main__":
