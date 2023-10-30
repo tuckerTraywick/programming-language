2e4adfe3155872406e765364158c7c7ca2a741bd
Tucker Traywick <tucker.traywick@gmail.com>
2023-10-30 21:47:56 +0000

    Got basic lexing working.

--- source/main.py
+++ source/main.py
@@ -9,8 +9,85 @@ class TokenType(Enum):
     CHARACTER = auto()
     STRING = auto()
     IDENTIFIER = auto()
+
+    PACKAGE = auto()
+    IMPORT = auto()
+    PUB = auto()
+    VAR = auto()
+    FUNC = auto()
+    IMPL = auto()
+    STRUCT = auto()
+    CASES = auto()
+    ABSTRACTS = auto()
+    PASS = auto()
+    RETURN = auto()
+    YIELD = auto()
+    BREAK = auto()
+    CONTINUE = auto()
+    IF = auto()
+    ELSE = auto()
+    SWITCH = auto()
+    CASE = auto()
+    DEFAULT = auto()
+    FOR = auto()
+    IN = auto()
+    FROM = auto()
+    UNTIL = auto()
+    THRU = auto()
+    BY = auto()
+    DO = auto()
+    WHILE = auto()
+    AS = auto()
+    IS = auto()
+    AND = auto()
+    OR = auto()
+    XOR = auto()
+    NOT = auto()
+    TRUE = auto()
+    FALSE = auto()
+ 
+    INCREMENT = auto()
+    PLUS_EQUALS = auto()
     PLUS = auto()
+    DECREMENT = auto()
+    MINUS_EQUALS = auto()
+    ARROW = auto()
     MINUS = auto()
+    TIMES_EQUALS = auto()
+    TIMES = auto()
+    DIVIDE_EQUALS = auto()
+    DIVIDE = auto()
+    MODULUS_EQUALS = auto()
+    MODULUS = auto()
+    BITWISE_AND_EQUALS = auto()
+    BITWISE_AND = auto()
+    BITWISE_OR_EQUALS = auto()
+    BITWISE_OR = auto()
+    BITWISE_XOR_EQUALS = auto()
+    BITWISE_XOR = auto()
+    BITWISE_NOT_EQUALS = auto()
+    BITWISE_NOT = auto()
+    LEFT_SHIFT_EQUALS = auto()
+    LEFT_SHIFT = auto()
+    LESS_EQUAL = auto()
+    LESS = auto()
+    RIGHT_SHIFT_EQUALS = auto()
+    RIGHT_SHIFT = auto()
+    GREATER_EQUAL = auto()
+    GREATER = auto()
+    EQUAL = auto()
+    FAT_ARROW = auto()
+    ASSIGN = auto()
+    NOT_EQUAL = auto()
+    DOT = auto()
+    COMMA = auto()
+    SEMICOLON = auto()
+    LEFT_PARENTHESIS = auto()
+    RIGHT_PARENTHESIS = auto()
+    LEFT_BRACKET = auto()
+    RIGHT_BRACKET = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
 
 
 # Represents a token. Maps to a segment of the input text.
@@ -21,49 +98,191 @@ class Token:
     end: int # The index of the end of the token in the text.
 
 
-# Splits a string into tokens.
-def lex(text: str) -> list[Token]:
-    # Every non-alphanumeric character.
-    symbols = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
-    # A map of token strigs to their operator types.
-    operators = {
-        "+": TokenType.PLUS,
-        "-": TokenType.MINUS,
-    }
-
-    tokens: list[Token] = []
-    currentToken: Token = None
-    for i, char in enumerate(text):
-        # If not currently lexing a token, figure out the type of the current token.
-        if currentToken is None:
-            if char.isspace():
-                continue
-            elif char.isdigit():
-                currentToken = Token(TokenType.NUMBER, i, i)
-            elif char.isalpha() or char == "_":
-                currentToken = Token(TokenType.IDENTIFIER, i, i)
-            elif char in symbols:
-                currentToken = Token(None, i, i)
-            else:
-                currentToken = Token(TokenType.INVALID, i, i)
-        # If in the middle of lexing a token, try to keep lexing it.
-        elif currentToken.type == TokenType.NUMBER and char.isdigit() \
-             or currentToken.type == TokenType.IDENTIFIER and (char.isalnum() or char == '_') \
-             or currentToken.type == None and (char in symbols):
-                currentToken.end = i
-            
-
-# Splits the file at `path` into tokens.
-def lexFile(path: str) -> list[Token]:
+# Reads the file at `path` into a string.
+def readFile(path: str) -> str:
     file = open(path, "r")
     text = file.read()
     file.close()
+    return text
+
+
+# Splits a string into tokens.
+def lex(text: str) -> list[Token]:
+    # A map of strings to the keywords they represent.
+    keywords = {
+        "package": TokenType.PACKAGE,
+        "import": TokenType.IMPORT,
+        "pub": TokenType.PUB,
+        "var": TokenType.VAR,
+        "func": TokenType.FUNC,
+        "impl": TokenType.IMPL,
+        "struct": TokenType.STRUCT,
+        "cases": TokenType.CASES,
+        "abstracts": TokenType.ABSTRACTS,
+        "pass": TokenType.PASS,
+        "return": TokenType.RETURN,
+        "yield": TokenType.YIELD,
+        "break": TokenType.BREAK,
+        "continue": TokenType.CONTINUE,
+        "if": TokenType.IF,
+        "else": TokenType.ELSE,
+        "switch": TokenType.SWITCH,
+        "case": TokenType.CASE,
+        "default": TokenType.DEFAULT,
+        "for": TokenType.FOR,
+        "in": TokenType.IN,
+        "from": TokenType.FROM,
+        "until": TokenType.UNTIL,
+        "thru": TokenType.THRU,
+        "by": TokenType.BY,
+        "do": TokenType.DO,
+        "while": TokenType.WHILE,
+        "as": TokenType.AS,
+        "is": TokenType.IS,
+        "and": TokenType.AND,
+        "or": TokenType.OR,
+        "xor": TokenType.XOR,
+        "not": TokenType.NOT,
+        "true": TokenType.TRUE,
+        "false": TokenType.FALSE,
+    }
+    # A map of strings to the operators they represent.
+    operators = {
+        "++": TokenType.INCREMENT,
+        "+=": TokenType.PLUS_EQUALS,
+        "+": TokenType.PLUS,
+        "--": TokenType.DECREMENT,
+        "-=": TokenType.MINUS_EQUALS,
+        "->": TokenType.ARROW,
+        "-": TokenType.MINUS,
+        "*=": TokenType.TIMES_EQUALS,
+        "*": TokenType.TIMES,
+        "/=": TokenType.DIVIDE_EQUALS,
+        "/": TokenType.DIVIDE,
+        "%=": TokenType.MODULUS_EQUALS,
+        "%": TokenType.MODULUS,
+        "&=": TokenType.BITWISE_AND_EQUALS,
+        "&": TokenType.BITWISE_AND,
+        "|=": TokenType.BITWISE_OR_EQUALS,
+        "|": TokenType.BITWISE_OR,
+        "^=": TokenType.BITWISE_XOR_EQUALS,
+        "^": TokenType.BITWISE_XOR,
+        "~=": TokenType.BITWISE_NOT_EQUALS,
+        "~": TokenType.BITWISE_NOT,
+        "<<=": TokenType.LEFT_SHIFT_EQUALS,
+        "<<": TokenType.LEFT_SHIFT,
+        "<=": TokenType.LESS_EQUAL,
+        "<": TokenType.LESS,
+        ">>=": TokenType.RIGHT_SHIFT_EQUALS,
+        ">>": TokenType.RIGHT_SHIFT,
+        ">=": TokenType.GREATER_EQUAL,
+        ">": TokenType.GREATER,
+        "==": TokenType.EQUAL,
+        "=>": TokenType.FAT_ARROW,
+        "=": TokenType.ASSIGN,
+        "!=": TokenType.NOT_EQUAL,
+        ".": TokenType.DOT,
+        ",": TokenType.COMMA,
+        ";": TokenType.SEMICOLON,
+        "(": TokenType.LEFT_PARENTHESIS,
+        ")": TokenType.RIGHT_PARENTHESIS,
+        "[": TokenType.LEFT_BRACKET,
+        "]": TokenType.RIGHT_BRACKET,
+        "{": TokenType.LEFT_BRACE,
+        "}": TokenType.RIGHT_BRACE,
+    }
+    # Every non-alphanumeric character.
+    symbols = "`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+    # The start of a line comment.
+    lineComment = "#"
+
+    tokens: list[Token] = []
+    i: int = 0
+    while i < len(text):
+        # Skip whitespace.
+        if text[i].isspace():
+            i += 1
+        # Skip comments.
+        elif text[i] == lineComment:
+            while i < len(text) and text[i] != "\n":
+                i += 1
+        # Try to lex a character literal.
+        elif text[i] == "'":
+            # TODO: Account for escapes and check length.
+            start: int = i
+            # Skip the first quote.
+            i += 1
+            # Skip the body of the string.
+            while i < len(text) and text[i] != "'":
+                i += 1
+            # Skip the last quote.
+            if i < len(text) and text[i] == "'":
+                i += 1
+                tokens.append(Token(TokenType.CHARACTER, start, i))
+            else:
+                tokens.append(Token(TokenType.INVALID, start, i))
+                break
+        # Try to lex a string literal.
+        elif text[i] == '"':
+            # TODO: Account for escapes and check length.
+            start: int = i
+            # Skip the first quote.
+            i += 1
+            # Skip the body of the string.
+            while i < len(text) and text[i] != '"':
+                i += 1
+            # Skip the last quote.
+            if i < len(text) and text[i] == '"':
+                i += 1
+                tokens.append(Token(TokenType.CHARACTER, start, i))
+            else:
+                tokens.append(Token(TokenType.INVALID, start, i))
+                break
+        # Try to lex a number.
+        elif text[i].isdigit():
+            start: int = i
+            while i < len(text) and text[i].isdigit():
+                i += 1
+            tokens.append(Token(TokenType.NUMBER, start, i))
+        # Try to lex an identifier or keyword.
+        elif text[i].isalpha() or text[i] == "_":
+            start: int = i
+            while i < len(text) and (text[i].isalnum() or text[i] == "_"):
+                i += 1
+            tokens.append(Token(keywords.get(text[start:i-1], TokenType.IDENTIFIER), start, i))
+        # Try to lex an operator.
+        elif text[i] in symbols:
+            # Try to lex each operator.
+            for name, type in operators.items():
+                if text.startswith(name, i):
+                    tokens.append(Token(type, i, i + len(name)))
+                    i += len(name)
+                    break
+            # If it's not an operator, it's an invalid token.
+            else:
+                # TODO: Find the length of the invalid token.
+                tokens.append(Token(TokenType.INVALID, i, i))
+                break # Break out of the outer while loop.
+        # If none of the above succeed, the token is invalid.
+        else:
+            tokens.append(Token(TokenType.INVALID, i, i))
+            break
+
+    return tokens
+
+
+# Splits the file at `path` into tokens.
+def lexFile(path: str) -> list[Token]:
+    text = readFile(path)
     return lex(text)
 
 
 def main():
     path = "source/example.txt"
-    print(lexFile(path))
+    text = readFile(path)
+    tokens = lex(text)
+    for i, token in enumerate(tokens):
+        print(f"{i:<3} {token} '{text[token.start:token.end]}'")
 
 
 if __name__ == "__main__":
