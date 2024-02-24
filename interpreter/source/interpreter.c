cad2007eb00804d3390abbe9876d604051fa5575
Tucker Traywick <tucker.traywick@gmail.com>
2024-02-23 23:19:30 -0500

    Refactored.

--- interpreter/source/interpreter.c
+++ interpreter/source/interpreter.c
@@ -12,20 +12,56 @@ struct Interpreter {
     uint8_t *ip;
     uint8_t *fp;
     uint8_t *sp;
+    bool keepRunning;
 };
+
+// Gets the pointer to the source for a copy instruction.
+static uint8_t *getSource(struct Interpreter *interpreter, uint8_t addressingMode) {
+    uint8_t *source = NULL;
+    switch ((addressingMode&0b11110000) >> 4) {
+        case IMMEDIATE:
+            source = interpreter->ip;
+            ++interpreter->ip;
+            break;
+        case STACK_TOP:
+            source = interpreter->sp - 1;
+            break;
+        default:
+            assert(0 && "Invalid opcode.");
+            interpreter->keepRunning = false;
+            break;
+    }
+    return source;
+}
+
+// Gets the pointer to the desitination of a copy instruction.
+static uint8_t *getDestination(struct Interpreter *interpreter, uint8_t addressingMode) {
+    uint8_t *destination = NULL;
+    switch (addressingMode&0b00001111) {
+        case STACK_TOP:
+            destination = interpreter->sp;
+            ++interpreter->sp;
+            break;
+        default:
+            assert(0 && "Invalid opcode.");
+            interpreter->keepRunning = false;
+            break;
+    }
+    return destination;
+}
 
 void run(uint8_t *code) {
     uint8_t *stack = malloc(STARTING_STACK_SIZE);
-
     struct Interpreter interpreter = {
-        .stack = malloc(STARTING_STACK_SIZE),
+        .stack = stack,
         .ip = code,
         .fp = stack,
         .sp = stack,
+        .keepRunning = true,
     };
+    assert(stack && "`malloc()` failed.");
 
-    bool keepRunning = true;
-    while (keepRunning) {
+    while (interpreter.keepRunning) {
         uint8_t opcode = *interpreter.ip;
         ++interpreter.ip;
 
@@ -36,38 +72,15 @@ void run(uint8_t *code) {
 
             case HALT:
                 // Exits the program.
-                keepRunning = false;
+                interpreter.keepRunning = false;
                 break;
 
             case COPY8:
                 // Copies a source to a destination.
                 uint8_t addressingMode = *interpreter.ip;
                 ++interpreter.ip;
-
-                uint8_t *source = NULL;
-                switch ((addressingMode&0b11110000) >> 4) {
-                    case IMMEDIATE:
-                        source = interpreter.ip;
-                        ++interpreter.ip;
-                        break;
-                    default:
-                        assert(0 && "Invalid opcode.");
-                        keepRunning = false;
-                        break;
-                }
-
-                uint8_t *destination = NULL;
-                switch (addressingMode&0b00001111) {
-                    case STACK_TOP:
-                        destination = interpreter.sp;
-                        ++interpreter.sp;
-                        break;
-                    default:
-                        assert(0 && "Invalid opcode.");
-                        keepRunning = false;
-                        break;
-                }
-
+                uint8_t *source = getSource(&interpreter, addressingMode);
+                uint8_t *destination = getDestination(&interpreter, addressingMode);
                 *destination = *source;
                 break;
 
@@ -79,10 +92,16 @@ void run(uint8_t *code) {
                 
             default:
                 assert(0 && "Invalid opcode.");
-                keepRunning = false;
+                interpreter.keepRunning = false;
                 break;
         }
     }
+
+    printf("\nstack:\n");
+    for (uint8_t *byte = interpreter.stack; byte <= interpreter.sp; ++byte) {
+        printf("%d ", *byte);
+    }
+    printf("\n");
 
     free(stack);
 }
--- interpreter/source/main.c
+++ interpreter/source/main.c
@@ -4,13 +4,11 @@
 
 static uint8_t code[] = {
     COPY8, MODE(IMMEDIATE, STACK_TOP), 255,
-    COPY8, MODE(IMMEDIATE, STACK_TOP), 123,
-    PRINT8,
+    COPY8, MODE(STACK_TOP, STACK_TOP),
     PRINT8,
     HALT,
 };
 
 int main(void) {
     run(code);
-    printf("Done.\n");
 }
