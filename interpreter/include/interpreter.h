807498cabe67f53816a6b9fa4d9e923fa347f1c6
Tucker <tucker.traywick@gmail.com>
2024-03-16 00:56:01 -0400

    Simplified interface.

--- interpreter/include/interpreter.h
+++ interpreter/include/interpreter.h
@@ -3,36 +3,13 @@
 
 #include <stdint.h>
 
-#define STACK_SIZE 1024*1024 // 1MB
+#define STACK_SIZE 5*1024*1024 // 5MB
 
-// Combines `source` and `dest` into one byte representing the addressing mode for a copy
-// instruction. The source is in the first 4 bits, the destination is in the last 4 bits.
-#define MODE(source, dest) (((uint8_t)(source) << 4) | (uint8_t)(dest))
-
-// Represents the addressing mode of the source/destination for a copy.
-enum AddressingMode {
-    IMMEDIATE,
-    POINTER,
-    STACK_TOP,
-    STACK_OFFSET,
-    LOCAL_OFFSET,
-    ARGUMENT_OFFSET,
-    STACK_TOP_POINTER,
-    STACK_OFFSET_POINTER,
-    LOCAL_OFFSET_POINTER,
-    ARGUMENT_OFFSET_POINTER,
-};
-
-// Represents the first byte of an opcode.
+// Represents the operation being performed by an isntruction.
 enum Opcode {
     NOOP,
 
     HALT,
-
-    COPY8,
-    COPY16,
-    COPY32,
-    COPY64,
 
     ADDU8,
     ADDU16,
