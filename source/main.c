4e0d555202f20244b531d8ca0c0e265ccd37d2ee
Tucker Traywick <tucker.traywick@gmail.com>
2024-06-23 01:20:43 +0000

    Test saving symbol table.

--- source/main.c
+++ source/main.c
@@ -7,27 +7,22 @@
 #include "interpreter.h"
 
 int main(void) {
-    // struct Object object = ObjectCreate(8*1024, 100);
-    // FILE *file = fopen("example.bin", "wb+");
+    struct Object object = ObjectCreate(1024, 100);
     
-    // ObjectWriteToFile(&object, file);
-    // ObjectDestroy(&object);
-    
-    // object = ObjectReadFromFile(file);
-    // ObjectPrint(&object);
-    
-    // ObjectDestroy(&object);
-    // fclose(file);
-    SymbolTable table = ListCreate(10, sizeof (struct Symbol));
-    ListChar strings = ListCreate(100, 1);
-    struct Symbol symbol = {.category=MUTABLE_DATA};
+    struct Symbol symbol = {.category = MUTABLE_DATA};
+    SymbolTableSetSymbol(&object.symbolTable, &object.strings, "foo", &symbol);
+    SymbolTableSetSymbol(&object.symbolTable, &object.strings, "bar", &symbol);
 
-    SymbolTableSetSymbol(&table, &strings, "foo", &symbol);
-    SymbolTableSetSymbol(&table, &strings, "bar", &symbol);
-    struct Symbol *result = SymbolTableGetSymbol(&table, &strings, "bar");
-    printf("name = %s\n", (result) ? (char*)strings.elements + result->name : "NULL");
+    FILE *file = fopen("example.bin", "wb+");
+    ObjectWriteToFile(&object, file);
+    ObjectDestroy(&object);
+    object = ObjectReadFromFile(file);
+    ObjectPrint(&object);
+    fclose(file);
 
-    ListDestroy(&table);
-    ListDestroy(&strings);
+    struct Symbol *result = SymbolTableGetSymbol(&object.symbolTable, &object.strings, "foo");
+    printf("name = %s\n", (result) ? (char*)object.strings.elements + result->name : "NULL");
+
+    ObjectDestroy(&object);
     return 0;
 }
