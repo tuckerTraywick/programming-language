24d0584f11d3584bf1de057c0ca2a2c0d52a362c
Tucker Traywick <tucker.traywick@gmail.com>
2024-07-28 23:08:44 -0400

    Added more keywords and operators.

--- source/main.c
+++ source/main.c
@@ -2,7 +2,14 @@
 #include "lexer.h"
 
 int main(void) {
-    char *text = "123 1-\n  \n\r\n\r \t a";
+    char *text =
+    "pub package example\n"
+    "pub func fib(n int32) int32 {\n"
+    "    if n < 1 {\n"
+    "        return n\n"
+    "    }\n"
+    "    return fib(n - 1) + fib(n - 2)\n"
+    "}";
     LexingResult result = lex(text);
     LexingResultPrint(&result);
     LexingResultDestroy(&result);
