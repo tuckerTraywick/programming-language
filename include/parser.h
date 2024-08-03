2817503c4bdbd9777c0baec6fdf6149deb482456
Tucker Traywick <tucker.traywick@gmail.com>
2024-08-03 00:29:01 -0400

    Refactor.

--- include/parser.h
+++ include/parser.h
@@ -9,11 +9,12 @@ typedef List SyntaxNodeList;
 
 // The type of thing in the grammar a node represents.
 typedef enum SyntaxNodeType {
-	INVALID_SYNTAX = TOKEN_TYPE_COUNT,
+	INVALID_SYNTAX,
 	MISSING_PACKAGE_NAME,
 	MISSING_SUBPACKAGE_NAME,
 	EXPECTED_SEMICOLON,
 
+	TOKEN,
 	PROGRAM,
 	STATEMENT,
 	EXPRESSION,
@@ -26,6 +27,7 @@ typedef struct SyntaxNode {
 	SyntaxNodeType type;
 	struct SyntaxNode *sibling;
 	struct SyntaxNode *child;
+	Token *token; // NULL if the node doesn't represent a token.
 } SyntaxNode;
 
 // The output of the parser. Must be destroyed by `ParsingResultDestroy()` after use.
