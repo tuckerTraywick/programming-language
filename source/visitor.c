c28d7c1b326625b62109d4f5b1507309ef1bb406
Tucker Traywick <tucker.traywick@gmail.com>
2026-05-28 11:31:41 -0400

    Implemented symbol table constructor and destructor.

--- source/visitor.c
+++ source/visitor.c
@@ -1,1 +1,35 @@
+#include <stddef.h>
 #include "visitor.h"
+#include "map.h"
+
+struct symbol_table symbol_table_create(size_t buckets_capacity, size_t keys_capacity) {
+	struct symbol_table table = {
+		.handles = map_create(buckets_capacity, sizeof *table.handles, keys_capacity),
+	};
+	if (!table.handles) {
+		goto error1;
+	}
+	table.modules = map_create(buckets_capacity, sizeof *table.modules, keys_capacity);
+	if (!table.modules) {
+		goto error2;
+	}
+	table.variables = map_create(buckets_capacity, sizeof *table.variables, keys_capacity);
+	if (!table.variables) {
+		goto error3;
+	}
+	return table;
+
+error3:
+	map_destroy(&table.modules);
+error2:
+	map_destroy(&table.handles);
+error1:
+	return (struct symbol_table){0};
+}
+
+void symbol_table_destroy(struct symbol_table *table) {
+	map_destroy(&table->handles);
+	map_destroy(&table->modules);
+	map_destroy(&table->variables);
+	*table = (struct symbol_table){0};
+}
--- tests/main.c
+++ tests/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include "test.h"
+#include "visitor.h"
+
+void test_symbol_table_create_and_destroy(void) {
+	struct symbol_table table = symbol_table_create(10, 10);
+	assert(table.handles);
+	symbol_table_destroy(&table);
+	assert(!table.handles);
+}
 
 int main(void) {
+	begin_testing();
+		run_test(test_symbol_table_create_and_destroy);
+	end_testing();
 	return 0;
 }
